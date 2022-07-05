be1d6ddef3a51e778855dfbd7da09bd1ced0b714
Michał Wlocka <mwlocka@fis-sst.pl>
2022-07-05 12:35:46 +0200

    Initial commit

---
+++ src/test/java/com/fisproject/springlbd/SpringLbdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisproject.springlbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLbdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
