7aecd8fab2d933ccd37aa65a2b2be93c0990b5a4
michalw <michalwlocka@gmail.com>
2022-07-15 12:25:17 +0200

    dodane MethodArgumentTypeMismatchException

--- src/main/java/com/fisproject/springlbd/controller/ControllersAdvice.java
+++ src/main/java/com/fisproject/springlbd/controller/ControllersAdvice.java
@@ -6,6 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 
 /**
@@ -16,7 +17,14 @@ public class ControllersAdvice {
     @ExceptionHandler(MissingServletRequestParameterException.class)
     public ResponseEntity<StandardResponse> handleMissingParams(MissingServletRequestParameterException e) {
         return ResponseEntity.badRequest().body(
-                new StandardResponse(HttpStatus.BAD_REQUEST, "", "Missing param '"+e.getParameterName()+"'."));
+                new StandardResponse(HttpStatus.BAD_REQUEST, "", "Missing param '"+e.getParameterName()+"'"));
+    }
+
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<StandardResponse> handleMethodArgumentTypeMismatch(MethodArgumentTypeMismatchException e) {
+
+        return ResponseEntity.badRequest().body(
+                new StandardResponse(HttpStatus.BAD_REQUEST, "", "Wrong param type for '"+e.getName()+"'"));
     }
 
 }
